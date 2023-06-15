6fdbabd7c6bc3d21be431a493eb56f1d2061b04e
Reflinders <74808818+Reflinders@users.noreply.github.com>
2023-06-15 10:06:23 -0500

    Update charcoal.lua
    removed some of the yanderedev moments and added the new method `Construct`

--- charcoal.lua
+++ charcoal.lua
@@ -1,9 +1,11 @@
 --/ ... Charcoal; a clean-up class
--- V.1.0; Last updated 5/19
+-- Made by @Reflinders on github
+-- V.1.0.1; Last updated 6/15
+-- 
 -- / ...
 
 local Charcoal = {}; Charcoal.__index = Charcoal
-export type Charchoal = typeof(setmetatable({}, Charcoal)) & {
+export type Cleaner = typeof(setmetatable({}, Charcoal)) & {
 	_items : {[number]:any?}
 }
 --/ ...
@@ -18,39 +20,47 @@ end
 function Charcoal:Handle(itemToHandle : any, items : {}, index : number)
 	local rblxCo = "RBXScriptConnection"
 	local typeOf = typeof(itemToHandle)
+	local methods
 	--/ ...
-	if typeOf == 'table' then
-		if itemToHandle.Destroy then
-			itemToHandle:Destroy()
-		elseif itemToHandle.Disconnect then
-			itemToHandle:Disconnect(); items[index] = nil
-		elseif itemToHandle.__char then
-			for _b, x in pairs(itemToHandle) do
-				local valid = {
-					Instance = true,
-					table = true,
-					['function'] = true,
-					[rblxCo] = true
-				}
-				if valid[typeof(x)] then
-					self:Handle(x)
-				else
-					itemToHandle[_b] = nil
+	methods = {
+		['table'] = function(item)
+			if item.Destroy then
+				item:Destroy()
+			elseif item.Disconnect then
+				item:Disconnect(); items[index] = nil
+			elseif item.__char then
+				for subIndex, subItem in pairs(item) do
+					if methods[typeof(subItem)] then
+						self:Handle(subItem, item, subIndex)
+					else
+						item[subIndex] = nil
+					end
 				end
 			end
+		end;
+		['Instance'] = function(item)
+			item:Destroy()
+		end;
+		['function'] = function(item)
+			item(); items[index] = nil
+		end;
+		[rblxCo] = function(item)
+			item:Disconnect()
 		end
-	elseif typeOf == 'Instance' then
-		itemToHandle:Destroy()
-	elseif typeOf == 'function' then
-		itemToHandle(); items[index] = nil
-	elseif typeOf == rblxCo then
-		itemToHandle:Disconnect()
+	}
+	local methodAvailable = methods[typeof(itemToHandle)]; if methodAvailable then
+		methodAvailable(itemToHandle)
 	end
+end
+function Charcoal:Construct(constructor : ()->(), ...) : {}|number
+	local newObject = constructor(...)
+	local index = self:Add(newObject)
+	return newObject, index
 end
 function Charcoal:Add(...) : number -- returns numid
 	local ids = {}
 	for _, item in ipairs({...}) do
-		assert(item ~= nil, "Charchoal: Error-- Item is nil")
+		assert(item ~= nil, "Charcoal: Error-- Item is nil")
 		local ind = #self._items + 1; if typeof(item) == 'table' and not item.__char and not item.Destroy then
 			warn("Charcoal: attempt to add object(table) without a .Destroy method or __char")	
 		end
@@ -77,13 +87,13 @@ end
 function Charcoal:IsA(type : string) : boolean 
 	return (type:lower() == 'charcoal')
 end 
-function Charcoal:Destroy() -- completely destroy 
-	self:Scorch(); self = nil
+function Charcoal:Destroy() -- scorch
+	self:Scorch()
 end
-function Charcoal.new() : Charchoal
+function Charcoal.new() : Cleaner
 	return setmetatable({
 		_items = {}
-	}  :: Charchoal, Charcoal)	
+	}, Charcoal) :: Cleaner
 end
 --/ ...
 return Charcoal
